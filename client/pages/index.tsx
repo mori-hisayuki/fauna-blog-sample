93dc12a261caf645b0c1e3aaf21bb79e4481da3c
hisayuki_mori <h.mori0206@gmail.com>
2022-10-21 10:46:05 +0900

    add Tailwind Css settings

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -18,8 +18,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
@@ -46,9 +45,7 @@ const Home: NextPage = () => {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
