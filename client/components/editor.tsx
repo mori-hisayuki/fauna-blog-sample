ac1d4b60c86173b9a8601e215713af41553cc8e4
hisayuki_mori <h.mori0206@gmail.com>
2022-10-22 20:27:45 +0900

    create Editor

--- client/components/editor.tsx
+++ client/components/editor.tsx
@@ -1,30 +1,149 @@
-import React from 'react'
 import { useEffect, useRef, useState } from 'react'
 import EditorJS from '@editorjs/editorjs'
 import Header from '@editorjs/header'
+import Image from '@editorjs/image'
+import Code from '@editorjs/code'
+import Paragraph from '@editorjs/paragraph'
+import Raw from '@editorjs/raw'
 import List from '@editorjs/list'
+import Checklist from '@editorjs/checklist'
 import Quote from '@editorjs/quote'
+import Warning from '@editorjs/warning'
+import Marker from '@editorjs/marker'
 import Delimiter from '@editorjs/delimiter'
 import InlineCode from '@editorjs/inline-code'
-import Marker from '@editorjs/marker'
-import Embed from '@editorjs/embed'
-import Image from '@editorjs/image'
-import Table from '@editorjs/table'
-import Warning from '@editorjs/warning'
-import Code from '@editorjs/code'
-import Checklist from '@editorjs/checklist'
 import LinkTool from '@editorjs/link'
-import Raw from '@editorjs/raw'
-import Paragraph from '@editorjs/paragraph'
+import Embed from '@editorjs/embed'
 import Codebox from '@bomdi/codebox'
+import Table from '@editorjs/table'
 import gql from 'graphql-tag'
 import apolloClient from '../lib/apolloClient'
 
 const Editor: React.FC = () => {
-  const editorRef = useRef(null)
+  const editorRef = useRef<EditorJS>()
   const [editorData, setEditorData] = useState(null)
-  const initEditor = () => {}
-  const handleSave = async () => {}
+
+  const initEditor = () => {
+    const editor = new EditorJS({
+      holderId: 'editorjs',
+      tools: {
+        header: {
+          class: Header,
+          inlineToolbar: ['marker', 'link'],
+          config: {
+            placeholder: 'Enter a header',
+            levels: [1, 2, 3, 4, 5, 6],
+            defaultLevel: 3
+          },
+          shortcut: 'CMD+SHIFT+H'
+        },
+        image: Image,
+        code: Code,
+        paragraph: {
+          class: Paragraph,
+          inlineToolbar: true
+        },
+        raw: Raw,
+        list: {
+          class: List,
+          inlineToolbar: true,
+          shortcut: 'CMD+SHIFT+L'
+        },
+        checklist: {
+          class: Checklist,
+          inlineToolbar: true
+        },
+        quote: {
+          class: Quote,
+          inlineToolbar: true,
+          config: {
+            quotePlaceholder: 'Enter a quote',
+            captionPlaceholder: "Quote's author"
+          },
+          shortcut: 'CMD+SHIFT+O'
+        },
+        warning: Warning,
+        marker: {
+          class: Marker,
+          shortcut: 'CMD+SHIFT+M'
+        },
+        delimiter: Delimiter,
+        inlineCode: {
+          class: InlineCode,
+          shortcut: 'CMD+SHIFT+C'
+        },
+        linkTool: LinkTool,
+        embed: Embed,
+        codebox: Codebox,
+        table: {
+          class: Table,
+          inlineToolbar: true,
+          shortcut: 'CMD+ALT+T'
+        }
+      },
+      // autofocus: true,
+      placeholder: 'Write your story...',
+      data: {
+        blocks: [
+          {
+            type: 'header',
+            data: {
+              text: 'New blog post title here....',
+              level: 2
+            }
+          },
+          {
+            type: 'paragraph',
+            data: {
+              text: 'Blog post introduction here....'
+            }
+          }
+        ]
+      },
+      onReady: () => {
+        console.log('Editor.js is ready to work!')
+        editorRef.current = editor
+      },
+      onChange: () => {
+        console.log('Content was changed')
+      }
+    })
+  }
+  const handleSave = async () => {
+    // 1. GQL mutation to create a blog post in Fauna
+    const CREATE_POST = gql`
+      mutation CreatePost($content: String!, $slug: String!) {
+        createPost(data: { published: true, content: $content, slug: $slug }) {
+          content
+          slug
+          published
+        }
+      }
+    `
+    const outputData = await editorRef.current.save()
+    // 3. Get blog title to create a slug
+    let title = 'no title'
+    for (let i = 0; i < outputData.blocks.length; i++) {
+      if (
+        outputData.blocks[i].type === 'header' &&
+        outputData.blocks[i].data.level === 2
+      ) {
+        title = outputData.blocks[i].data.text
+        break
+      }
+    }
+    const slug = title
+      .toLowerCase()
+      .replace(/ /g, '-')
+      .replace(/[^\w-]+/g, '')
+    await apolloClient.mutate({
+      mutation: CREATE_POST,
+      variables: {
+        content: JSON.stringify(outputData),
+        slug: slug
+      }
+    })
+  }
 
   useEffect(() => {
     if (!editorRef.current) {
