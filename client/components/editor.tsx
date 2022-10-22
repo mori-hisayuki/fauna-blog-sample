12eb3adaee0df484e1d9da4536a3bf530c9929e9
hisayuki_mori <h.mori0206@gmail.com>
2022-10-22 16:19:09 +0900

    set default EditorComponent

--- client/components/editor.tsx
+++ client/components/editor.tsx
@@ -1,37 +1,36 @@
-import React from "react";
-import { useEffect, useRef, useState } from "react";
-import EditorJS from "@editorjs/editorjs";
-import Header from "@editorjs/header";
-import List from "@editorjs/list";
-import Quote from "@editorjs/quote";
-import Delimiter from "@editorjs/delimiter";
-import InlineCode from "@editorjs/inline-code";
-import Marker from "@editorjs/marker";
-import Embed from "@editorjs/embed";
-import Image from "@editorjs/image";
-import Table from "@editorjs/table";
-import Warning from "@editorjs/warning";
-import Code from "@editorjs/code";
-import Checklist from "@editorjs/checklist";
-import LinkTool from "@editorjs/link";
-import Raw from "@editorjs/raw";
-import Paragraph from "@editorjs/paragraph";
-import Codebox from "@bomdi/codebox";
-import gql from "graphql-tag";
-import apolloClient from "../lib/apolloClient";
+import React from 'react'
+import { useEffect, useRef, useState } from 'react'
+import EditorJS from '@editorjs/editorjs'
+import Header from '@editorjs/header'
+import List from '@editorjs/list'
+import Quote from '@editorjs/quote'
+import Delimiter from '@editorjs/delimiter'
+import InlineCode from '@editorjs/inline-code'
+import Marker from '@editorjs/marker'
+import Embed from '@editorjs/embed'
+import Image from '@editorjs/image'
+import Table from '@editorjs/table'
+import Warning from '@editorjs/warning'
+import Code from '@editorjs/code'
+import Checklist from '@editorjs/checklist'
+import LinkTool from '@editorjs/link'
+import Raw from '@editorjs/raw'
+import Paragraph from '@editorjs/paragraph'
+import Codebox from '@bomdi/codebox'
+import gql from 'graphql-tag'
+import apolloClient from '../lib/apolloClient'
 
-export const Editor: React.FC = () => {
-  const editorRef = useRef(null);
-  const [editorData, setEditorData] = useState(null);
+const Editor: React.FC = () => {
+  const editorRef = useRef(null)
+  const [editorData, setEditorData] = useState(null)
   const initEditor = () => {}
   const handleSave = async () => {}
 
   useEffect(() => {
-    if(!editorRef.current) {
-        initEditor();
+    if (!editorRef.current) {
+      initEditor()
     }
-  }, []);
-
+  }, [])
 
   return (
     <div>
@@ -48,3 +47,9 @@ export const Editor: React.FC = () => {
     </div>
   )
 }
+
+/**
+ * dynamic import時にdefaultが付与されている必用がある\
+ * (`LoaderComponent`型が`Promise<React.ComponentType<P> | { default: React.ComponentType<P> }`)
+ */
+export default Editor
